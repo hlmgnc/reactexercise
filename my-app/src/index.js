3f98f714847b1d5275146974547973be84c3f936
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-10-28 11:53:34 +0100

    Work on some JSX rules

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,22 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 function Greeting() {
-  return <h4>my first react component</h4>
+  return (
+    <>
+      <article>
+        <h4>Hello React</h4>
+        <ul>
+          <img src="" alt="" />
+          <input type="text" name="" id="" />
+        </ul>
+      </article>
+      <div>hey hey</div>
+    </>
+  )
 }
+
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h4', {}, 'HELLO WORLD'))
+// }
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
