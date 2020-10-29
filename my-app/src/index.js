f00c7abeb0b98c1bdc6179cb8a35552a5527f306
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-10-29 15:33:43 +0100

    Exercise nested components and css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './index.css'
 
-function Greeting() {
+function BookList() {
   return (
-    <>
-      <article>
-        <h4>Hello React</h4>
-        <ul>
-          <img src="" alt="" />
-          <br />
-          <input type="text" name="" id="" />
-        </ul>
-      </article>
-      <div>hey hey</div>
-    </>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
-// const Greeting = () => {
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  )
+}
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81Q8O-3Hq1L._AC_UL200_SR200,200_.jpg" alt="" />
+)
+
+const Title = () => <h1>The Food Lab</h1>
+const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>Kenji Lopez Alt</h4>
+// const Person = () => <h2> hello</h2>
+// const Message = () => {
+//   return <p>heyyoooo</p>
+// }
+// const BookList = () => {
 //   return React.createElement('div', {}, React.createElement('h4', {}, 'HELLO WORLD'))
 // }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+ReactDom.render(<BookList />, document.getElementById('root'))
