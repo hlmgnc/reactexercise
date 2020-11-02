1f6a4eb3cd7dc38be08883459cd7d4e1f235f62b
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-11-02 12:16:08 +0100

    Exercise props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,44 +1,48 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
+const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Q8O-3Hq1L._AC_UL200_SR200,200_.jpg',
+    title: 'The Food Lab',
+    author: 'Kenji Lopez Alt',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg',
+    title: 'Greenlights',
+    author: 'Matthew McConaughey',
+  },
+]
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        const { img, title, author } = book
+        return <Book key={book.id} {...book}></Book>
+      })}
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
+  const { img, title, author } = props
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   )
 }
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/81Q8O-3Hq1L._AC_UL200_SR200,200_.jpg" alt="" />
-)
-
-const Title = () => <h1>The Food Lab</h1>
-const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>Kenji Lopez Alt</h4>
-// const Person = () => <h2> hello</h2>
-// const Message = () => {
-//   return <p>heyyoooo</p>
-// }
-// const BookList = () => {
-//   return React.createElement('div', {}, React.createElement('h4', {}, 'HELLO WORLD'))
-// }
 
 ReactDom.render(<BookList />, document.getElementById('root'))
